c1ab853e6eab166bd67b82a6cd37c960eb40d313
raosavarala <160120642+raosavarala@users.noreply.github.com>
2024-09-11 11:51:56 +0530

    Add files via upload

---
+++ index1/rao.php
@@ -0,0 +1,389 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style2.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
+
+    <title>romofie</title>
+</head>
+<body>
+  <div class="container">
+    <div class="row" style="text-align:center;">
+      <div class="col-sm-4"  style="margin-top:15px;">
+      <p> <span class="glyphicon glyphicon-envelope">demo@gmail.com</span></p>
+     </div>
+     <div class="col-sm-4">
+      <img src="img1/logo (1).png" alt="">
+     </div>
+     <div class="col-sm-4" style="margin-top:15px;">
+     <p> <span class="glyphicon glyphicon-bag">
+      <button style="background-color:skyblue;color:white;border-radius:10px;"> Order now</button></span></p>
+ 
+     </div>
+    </div>
+  </div>
+<nav class="navbar navbar-expand-sm ">
+  <div class="container-fluid" style="border-radius:30px;>
+    
+    <div class="collapse navbar-collapse" id="mynavbar" style="border-radius:50px;">
+      <ul class="navbar ">
+      <li class="nav">
+          <a class="nav-link" href="#" style="color:white;margin-left:60px;">HOME</a>
+        </li>
+        <li class="nav">
+          <a class="nav-link" href="#" style="color:white;">ABOUT</a>
+        </li>
+        <li class="nav">
+          <a class="nav-link" href="#" style="color:white;">PRODUCT</a>
+        </li>
+        <li class="nav">
+          <a class="nav-link" href="#" style="color:white;">FASHION</a>
+        </li>
+        <li class="nav">
+          <a class="nav-link" href="#" style="color:white;">NEWS</a>
+        </li>
+        <li class="nav">
+          <a class="nav-link" href="#" style="color:white;">CONTACT</a>
+        </li>
+      </ul>
+      <form class="d-flex">
+        <input class="form-control " type="text" placeholder="Search" style="width:350px;border-radius:40px;"> 
+      </form>
+    </div>
+  </div>
+</nav>
+<div class="row" style="background-image:url('img1/banner.jpg')">
+<div class="col-sm-7" style="">
+    <h1 style="color:blue;margin-top:100px;font-size:65px;margin-left:210px;">Romofyi</h1>
+    <h1 style="color:yellow;font-size:73px;margin-left:240px;">Trends 2019</h1>
+    <h3 style="margin-left:190px;">A huge fashion collectiob for ever</h3><br>
+    <button style="background-color:blue;color:white;width:120px;height:40px;border-radius:20px;margin-left:190px;">shop now</button>
+</div>
+<div class="col-sm-5">
+    <img src="img1/ban_img.png" width="400px" height="550px" >
+</div>
+</div>
+<DIV class="row">
+    <div class=" col-sm-2" style="background-color:yellow;text-align:center;">
+        <img src="img1/shoes (1).png" alt="">
+        <h4>shoe</h4>
+    </div>
+    <div class=" col-sm-2" style="background-color:skyblue;text-align:center;">
+        <img src="img1/underwear.png" alt="">
+        <h4>underware</h4>
+    </div>
+    <div class=" col-sm-2" style="background-color:yellow;text-align:center;">
+        <img src="img1/pent.png" alt="">
+        <h4>pant</h4>
+    </div>
+    <div class=" col-sm-2" style="background-color:skyblue;text-align:center;">
+        <img src="img1/t_shart.png" alt="">
+        <h4>shart</h4>
+    </div>
+    <div class=" col-sm-2" style="background-color:yellow;height:150px;text-align:center;">
+        <img src="img1/jakit.png" alt="">
+        <h4>jaket</h4>
+    </div>
+    <div class=" col-sm-2" style="background-color:skyblue;text-align:center;" >
+        <img src="img1/helbet.png" alt="">
+        <h4>helmat</h4>
+
+    </div>
+
+</DIV><br><br><br>
+<div style="background-image:url('img1/product_bg.jpg');background-repeat:no-repeat;background-size:cover;">
+<div style="margin-left:200px;">
+  <h2>Feature products</h2>
+  <hr style="border:5px solid yellow;margin-right:160px;">
+</div>
+<div class="container">
+<div class="row">
+  <div class="col-sm-1">
+</div>
+<div class="col-md-2" style="background-color:whitesmoke;margin:5px;height:200px;">
+  <img src="img1/shoes1.png" alt="">
+</div><br><br>
+<div class="col-md-2" style="background-color:whitesmoke;margin:5px;">
+  <img src="img1/shoes2.png" alt="">
+</div>
+<div class="col-md-2" style="background-color:whitesmoke;margin:4px;">
+  <img src="img1/shoes3.png" alt="">
+</div>
+<div class="col-md-2" style="background-color:whitesmoke;margin:5px;">
+  <img src="img1/shoes4.png" alt="">
+</div>
+<div class="col-md-2" style="background-color:whitesmoke;margin:5px;">
+  <img src="img1/shoes5.png" alt="">
+</div>
+<div class="col-md-1"></div>
+</div>
+
+
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-1">
+    </div>
+    <div class="col-md-2" style="margin:5px;">
+      <p>Short Openwork Cardigan<br>$120</p>
+    </div>
+    <div class="col-md-2" style="margin:5px;">
+      <p>Short Openwork Cardigan<br>$120</p>
+    </div>
+    <div class="col-md-2" style="margin:5px;">
+      <p>Short Openwork Cardigan<br>$120</p>
+    </div>
+    <div class="col-md-2" style="margin:5px;">
+      <p>Short Openwork Cardigan<br>$120</p>
+    </div>
+    <div class="col-md-2" style="margin:5px;">
+      <p>Short Openwork Cardigan<br>$120</p>
+    </div>
+    <div class="col-md-1">
+    </div>
+
+  </div>
+</div>
+<div class="container">
+<div class="row">
+  <div class="col-sm-1">
+</div>
+<div class="col-md-2" style="background-color:whitesmoke;margin:5px;height:200px;">
+  <img src="img1/tisat1.png" alt="">
+</div><br><br>
+<div class="col-md-2" style="background-color:whitesmoke;margin:5px;">
+  <img src="img1/tisat2.png" alt="">
+</div>
+<div class="col-md-2" style="background-color:whitesmoke;margin:4px;">
+  <img src="img1/tisat3.png" alt="">
+</div>
+<div class="col-md-2" style="background-color:whitesmoke;margin:5px;">
+  <img src="img1/tisat4.png" alt="">
+</div>
+<div class="col-md-2" style="background-color:whitesmoke;margin:5px;">
+  <img src="img1/tisat5.png" alt="">
+</div>
+<div class="col-md-1"></div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-1">
+    </div>
+    <div class="col-md-2" style="margin:5px;">
+      <p>Short Openwork Cardigan<br>$120</p>
+    </div>
+    <div class="col-md-2" style="margin:5px;">
+      <p>Short Openwork Cardigan<br>$120</p>
+    </div>
+    <div class="col-md-2" style="margin:5px;">
+      <p>Short Openwork Cardigan<br>$120</p>
+    </div>
+    <div class="col-md-2" style="margin:5px;">
+      <p>Short Openwork Cardigan<br>$120</p>
+    </div>
+    <div class="col-md-2" style="margin:5px;">
+      <p>Short Openwork Cardigan<br>$120</p>
+    </div>
+    <div class="col-md-1">
+    </div>
+
+  </div>
+</div>
+</div>
+<div class="container">
+<div class="row">
+  <div class="col-sm-1">
+</div>
+<div class="col-md-2" style="background-color:whitesmoke;margin:5px;height:200px;">
+  <img src="img1/mix1.png" alt="">
+</div><br><br>
+<div class="col-md-2" style="background-color:whitesmoke;margin:5px;">
+  <img src="img1/mix2.png" alt="">
+</div>
+<div class="col-md-2" style="background-color:whitesmoke;margin:4px;">
+  <img src="img1/mix3.png" alt="">
+</div>
+<div class="col-md-2" style="background-color:whitesmoke;margin:5px;">
+  <img src="img1/mix4.png" alt="">
+</div>
+<div class="col-md-2" style="background-color:whitesmoke;margin:5px;">
+  <img src="img1/mix5.png" alt="">
+</div>
+<div class="col-md-1"></div>
+</div>
+<div class="container">
+  <div class="row">
+    <div class="col-md-1">
+    </div>
+    <div class="col-md-2" style="margin:5px;">
+      <p>Short Openwork Cardigan<br>$120</p>
+    </div>
+    <div class="col-md-2" style="margin:5px;">
+      <p>Short Openwork Cardigan<br>$120</p>
+    </div>
+    <div class="col-md-2" style="margin:5px;">
+      <p>Short Openwork Cardigan<br>$120</p>
+    </div>
+    <div class="col-md-2" style="margin:5px;">
+      <p>Short Openwork Cardigan<br>$120</p>
+    </div>
+    <div class="col-md-2" style="margin:5px;">
+      <p>Short Openwork Cardigan<br>$120</p>
+    </div>
+    <div class="col-md-1">
+    </div>
+  </div>
+</div>
+</div>
+</div><br><br>
+<div style="text-align:center;">
+<button style="color:white;background-color:blue;width:140px;height:40px;border-radius:20px;">See More</button>
+</div><br><br><br>
+<div>
+  <img src="img1/fashion.jpg" width="100%">
+</div><br><br><br>
+<div style="margin-left:200px;">
+  <h2>Latest News</h2>
+  <hr style="border:5px solid yellow;margin-right:160px;height:auto;">
+</div>
+<div class="container">
+  <div class="row">
+  <div class="col-md-1"></div>
+  
+    
+    <div class="col-md-3">
+      <img src="img1/news_img1.jpg" style="width:400px;height:230px;">
+    </div>
+    <div class="col-md-2"></div>
+  
+      <div class="col-md-6"><br>
+      <h4>Specimen book. It has survived not only five</h4><br>
+      <button>  <p  style="margin: 0;display:inline;">7 july 2019</p></button>
+       <p style="margin-left: 300px;display:inline;" text-align="right"><button>like</button><button> comment</button></p><br><br>
+     
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a 
+          type specimen book.</p>
+
+    </div>
+  </div>
+</div><br>
+<div class="container">
+  <div class="row">
+  <div class="col-md-1"></div>
+  
+    
+    <div class="col-md-3">
+      <img src="img1/news_img2.jpg" style="width:400px;height:230px;">
+    </div>
+    <div class="col-md-2"></div>
+  
+      <div class="col-md-6"><br>
+      <h4>Specimen book. It has survived not only five</h4><br>
+      <button>  <p  style="margin: 0;display:inline;">7 july 2019</p></button>
+       <p style="margin-left: 300px;display:inline;" text-align="right"><button>like</button><button> comment</button></p><br><br>
+     
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a 
+          type specimen book.</p>
+
+    </div>
+  </div>
+</div><br>
+<div class="container">
+  <div class="row">
+  <div class="col-md-1"></div>
+  
+    
+    <div class="col-md-3">
+      <img src="img1/news_img3.jpg" style="width:400px;height:230px;">
+    </div>
+    <div class="col-md-2"></div>
+  
+      <div class="col-md-6"><br>
+      <h4>Specimen book. It has survived not only five</h4><br>
+     <button>  <p  style="margin: 0;display:inline;">7 july 2019</p></button>
+       <p style="margin-left: 300px;display:inline;" text-align="right"><button>like</button><button> comment</button></p><br><br>
+      <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+         Lorem Ipsum has been the industry's standard dummy text ever since the 1500s,
+          when an unknown printer took a galley of type and scrambled it to make a 
+          type specimen book.</p>
+
+    </div>
+  </div>
+</div><br><br>
+<div class="container">
+  <div class="row">
+    <div class="col-md-1"></div>
+    <div class="col-md-4">
+   <h3>Subscribe to the Newletter</h3>
+    </div>
+    <div class="col-md-1"></div>
+    <div class="col-md-6">
+    <input type="email" class="form-control" id="email" placeholder="Enter email" name="email" style="border:1px solid black;">
+<br>
+    <button style="background-color:black;color:white;width:100px;height:50px;border-radius:20px;float:right;">Subscribe</button>
+    </div>
+    <div class="col-md-1"></div>
+  </div>
+</div><br><br>
+<div class="container">
+<div class="row" style="text-align:center;">
+</body><div class="col-md-1"></div>
+<div class="col-md-3" style="background-color:whitesmoke;margin:15px;height:150px;padding-top:40px;">
+  <img src="img1/icon_mony.png" alt="">
+  <h6>Money Back</h6>
+</div>
+<div class="col-md-3" style="background-color:whitesmoke;margin:15px;padding-top:40px;">
+  <img src="img1/icon_gift.png" alt="">
+  <h6>Special Gifts</h6>
+</div>
+<div class="col-md-3" style="background-color:whitesmoke;margin:15px;padding-top:40px;">
+  <img src="img1/icon_car.png" alt="">
+  <h6>Free Shipping</h6>
+</div>
+<div class="col-md-2"></div>
+
+</div>
+</div>
+<div class="container-fluid" style="background-color:blue;color:white;margin-top:-100px;">
+  <div class="row" style="margin-left:70px;">
+    <div class="col-md-1"></div>
+    <div class="col-md-2" style="margin:15px;padding-top:130px;">
+      <h4>INFORMATION</h4><br>
+    <p>There are many variations of passages of Lorem Ipsum available,
+       but the majority have suffered alteration in some form, by injected humour, 
+       or randomised words which don't look even slightly bellevable</p>
+
+    </div>
+    <div class="col-md-2" style="margin:15px;padding-top:130px;">
+    <h3><small>MY ACCOUNT</small></h3><br>
+    <p>There are many variations of passages of Lorem Ipsum available,
+       but the majority have suffered alteration in some form, by injected humour, 
+       or randomised words which don't look even slightly bellevable</p>
+    </div>
+    <div class="col-md-2" style="margin:15px;padding-top:130px;">
+    <h4>ABOUT</h4><br>
+    <p>There are many variations of passages of Lorem Ipsum available,
+       but the majority have suffered alteration in some form, by injected humour, 
+       or randomised words which don't look even slightly bellevable</p>
+    </div>
+    <div class="col-md-2" style="margin:15px;padding-top:130px;">
+    <h4>CONTACTS</h4><br>
+    <p>There are many variations of passages of Lorem Ipsum available,
+       but the majority have suffered alteration in some form, by injected humour, 
+       or randomised words which don't look even slightly bellevable</p>
+    </div>
+    <div class="col-md-1"></div>
+
+  </div>
+</div><BR><BR>
+<h5 STYLE="TEXT-ALIGN:CENTER;">@2019 ALL RIGHTS RESERVED.DESIGNED BY FREE HTML TEMPLATES</h5><BR><br>
+</body>
+</html>
